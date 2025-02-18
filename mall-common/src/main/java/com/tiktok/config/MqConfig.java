341555131e47265b4ca7208cd085e3a99a03efba
laizhili <969414747@qq.com>
2025-02-18 15:05:02 +0800

    修复bug，确保服务引入SpringAMQP依赖时，才加载与RabbitMQ相关的配置

--- mall-common/src/main/java/com/tiktok/config/MqConfig.java
+++ mall-common/src/main/java/com/tiktok/config/MqConfig.java
@@ -1,11 +1,14 @@
 package com.tiktok.config;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(RabbitTemplate.class) //当项目引入了Spring AMQP依赖，才加载与RabbitMQ相关的配置
 public class MqConfig {
 
     /**
