e93f615a919c02476fe90935a198fd8477ec4575
qin-haonan <a793025902@163.com>
2025-02-28 14:24:34 +0800

    Dubbo 整合 Same-Token，实现 AuthService RPC 调用鉴权

---
+++ AuthService/src/main/java/com/tiktok/config/SaSameTokenRefreshTask.java
@@ -0,0 +1,21 @@
+package com.tiktok.config;
+
+import cn.dev33.satoken.same.SaSameUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Same-Token，定时刷新
+ */
+@Slf4j
+@Configuration
+public class SaSameTokenRefreshTask {
+    // 从 0 分钟开始 每隔 1 分钟执行一次 Same-Token
+    @Scheduled(cron = "0 0/1 * * * ? ")
+    public void refreshToken(){
+        log.info("AuthService: SameToken refresh");
+        SaSameUtil.refreshToken();
+    }
+}
+
